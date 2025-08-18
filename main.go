14156639b9b2600662acf16795539bbcc76e513b
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-08-18 21:28:55 +0530

    feat: uses go routines and channels while reading file advantages of using routines and channels - this introduces concurrency model of golang - instead of linear file read operation, concurrency helps to switch   context and is much faster than linear approach

--- main.go
+++ main.go
@@ -3,9 +3,45 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
+
+func getLinesChannel(f io.ReadCloser) <-chan string {
+	out := make(chan string, 1)
+
+	go func() {
+		defer f.Close()
+		defer close(out)
+
+		str := ""
+		for {
+			data := make([]byte, 8)
+			x, err := f.Read(data)
+			if err != nil {
+				break
+			}
+			data = data[:x] // take first 8 bytes present in data
+
+			// IndexByte: returns the index of first instance of '\n' in data
+			if i := bytes.IndexByte(data, '\n'); i != -1 {
+				str += string(data[:i])
+				data = data[i+1:]
+				out <- str // replacing the fmt.Printf
+				str = ""
+			}
+			str += string(data)
+		}
+
+		// also reads the empty lines at the last of file
+		if len(str) != 0 {
+			out <- str // replacing the fmt.Printf
+		}
+	}()
+
+	return out
+}
 
 func main() {
 	f, err := os.Open("messages.txt")
@@ -13,29 +49,8 @@ func main() {
 		log.Fatal("error opening file:", err)
 	}
 
-	defer f.Close()
-
-	str := ""
-	for {
-		data := make([]byte, 8)
-		x, err := f.Read(data)
-		if err != nil {
-			break
-		}
-		data = data[:x] // take first 8 bytes present in data
-
-		// IndexByte: returns the index of first instance of '\n' in data
-		if i := bytes.IndexByte(data, '\n'); i != -1 {
-			str += string(data[:i])
-			data = data[i+1:]
-			fmt.Printf("read: %s\n", str)
-			str = ""
-		}
-		str += string(data)
-	}
-
-	// also reads the empty lines at the last of file
-	if len(str) != 0 {
-		fmt.Printf("read: %s\n", str)
+	lines := getLinesChannel(f)
+	for line := range lines {
+		fmt.Printf("read: %s\n", line)
 	}
 }
