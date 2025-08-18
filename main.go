ef00a76b45c9e7eb5bf18de0382786859316bf92
GauraVashisth <miya.musashi.moto1645@gmail.com>
2025-08-18 17:26:11 +0530

    feat: reads `file` in 8-byte chunks and prints lines

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
@@ -14,13 +15,27 @@ func main() {
 
 	defer f.Close()
 
-	data := make([]byte, 8)
+	str := ""
 	for {
-		n, err := f.Read(data)
+		data := make([]byte, 8)
+		x, err := f.Read(data)
 		if err != nil {
 			break
 		}
+		data = data[:x] // take first 8 bytes present in data
 
-		fmt.Printf("read: %s\n", string(data[:n]))
+		// IndexByte: returns the index of first instance of '\n' in data
+		if i := bytes.IndexByte(data, '\n'); i != -1 {
+			str += string(data[:i])
+			data = data[i+1:]
+			fmt.Printf("read: %s\n", str)
+			str = ""
+		}
+		str += string(data)
+	}
+
+	// also reads the empty lines at the last of file
+	if len(str) != 0 {
+		fmt.Printf("read: %s\n", str)
 	}
 }
