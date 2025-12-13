e6468e6b4d464718f71766c709a9a1b34a1a3c43
gauravashisth <miya.musashi.moto1645@gmail.com>
2025-12-13 06:31:28 +0530

    perf(file read): using set buffer size(of 8) to read file contents

--- main.go
+++ main.go
@@ -1,16 +1,45 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"os"
 )
 
 func main() {
-	f, err := os.ReadFile("message.txt")
+	f, err := os.Open("message.txt")
 	if err != nil {
 		log.Fatalf("error: %v", err)
 	}
+	defer f.Close()
 
-	fmt.Printf("read: %s\n", string(f))
+	str := ""
+	// for file read operations, default buffer size is 4096 bytes
+	buf := make([]byte, 8)
+	for {
+		n, err := f.Read(buf)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatalf("read error: %v", err)
+		}
+
+		data := buf[:n]
+		if i := bytes.IndexByte(data, '\n'); i >= 0 {
+			str += string(data[:i])
+			fmt.Printf("read: %s\n", str)
+			str = ""
+			data = data[i+1:] // skips the newline
+
+		}
+		str += string(data)
+	}
+
+	// if there is any data left in the buffer, prints it
+	if len(str) > 0 {
+		fmt.Printf("read: %s\n", str)
+	}
 }
