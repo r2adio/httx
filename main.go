c9fc3f255184db96cb77d1e4e588feaaa53ea124
gauravashisth <miya.musashi.moto1645@gmail.com>
2025-12-18 23:45:00 +0530

    refactor(file read): spawn goroutine to read file contents

--- main.go
+++ main.go
@@ -8,6 +8,47 @@ import (
 	"os"
 )
 
+// getLinesReader returns a channel that will receive the lines of the file
+// io.ReadCloser: interface for reading files and closing them
+// os.File implements io.ReadCloser, to pass it directly to getLinesReader
+func getLinesReader(f io.ReadCloser) <-chan string {
+	ch := make(chan string, 1)
+	go func() {
+		defer f.Close()
+		defer close(ch)
+
+		str := ""
+		// for file read operations, default buffer size is 4096 bytes
+		buf := make([]byte, 8)
+		for {
+			n, err := f.Read(buf)
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("read error: %v", err)
+			}
+
+			data := buf[:n]
+			if i := bytes.IndexByte(data, '\n'); i >= 0 {
+				str += string(data[:i])
+				ch <- str
+				str = ""
+				data = data[i+1:] // skips the newline
+
+			}
+			str += string(data)
+		}
+
+		// if there is any data left in the buffer, prints it
+		if len(str) > 0 {
+			ch <- str
+		}
+	}()
+
+	return ch
+}
+
 func main() {
 	f, err := os.Open("message.txt")
 	if err != nil {
@@ -15,31 +56,8 @@ func main() {
 	}
 	defer f.Close()
 
-	str := ""
-	// for file read operations, default buffer size is 4096 bytes
-	buf := make([]byte, 8)
-	for {
-		n, err := f.Read(buf)
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatalf("read error: %v", err)
-		}
-
-		data := buf[:n]
-		if i := bytes.IndexByte(data, '\n'); i >= 0 {
-			str += string(data[:i])
-			fmt.Printf("read: %s\n", str)
-			str = ""
-			data = data[i+1:] // skips the newline
-
-		}
-		str += string(data)
-	}
-
-	// if there is any data left in the buffer, prints it
-	if len(str) > 0 {
-		fmt.Printf("read: %s\n", str)
+	lines := getLinesReader(f)
+	for line := range lines {
+		fmt.Printf("%s\n", line)
 	}
 }
