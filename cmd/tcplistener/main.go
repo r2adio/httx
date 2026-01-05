d6337dfa2c0732475e916316c30d81cbbc1dbc66
gauravashisth <miya.musashi.moto1645@gmail.com>
2026-01-05 19:38:25 +0530

    feat(cmd/tcplistener): prints request line in custom format

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,53 +1,12 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
-	"io"
 	"log"
 	"net"
+
+	"github.com/r2adio/httx/internal/request"
 )
-
-// getLinesReader returns a channel that will receive the lines of the file
-// io.ReadCloser: interface for reading files and closing them
-// os.File implements io.ReadCloser, to pass it directly to getLinesReader
-func getLinesReader(f io.ReadCloser) <-chan string {
-	ch := make(chan string, 1)
-	go func() {
-		defer f.Close()
-		defer close(ch)
-
-		str := ""
-		// for file read operations, default buffer size is 4096 bytes
-		buf := make([]byte, 8)
-		for {
-			n, err := f.Read(buf)
-			if err == io.EOF {
-				break
-			}
-			if err != nil {
-				log.Fatalf("read error: %v", err)
-			}
-
-			data := buf[:n]
-			if i := bytes.IndexByte(data, '\n'); i >= 0 {
-				str += string(data[:i])
-				ch <- str
-				str = ""
-				data = data[i+1:] // skips the newline
-
-			}
-			str += string(data)
-		}
-
-		// if there is any data left in the buffer, prints it
-		if len(str) > 0 {
-			ch <- str
-		}
-	}()
-
-	return ch
-}
 
 func main() {
 	l, err := net.Listen("tcp", ":42069")
@@ -62,8 +21,14 @@ func main() {
 		if err != nil {
 			log.Fatalf("error: %v", err)
 		}
-		for line := range getLinesReader(conn) {
-			fmt.Println(line)
+		r, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatalf("error: %v", err)
 		}
+
+		fmt.Printf("Request line:\n")
+		fmt.Printf("- Method: %s\n", r.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", r.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", r.RequestLine.HttpVersion)
 	}
 }
